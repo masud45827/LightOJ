5dae3fc65898721e684e45827de23c60b580cb02
masud45827 <60808203+masud45827@users.noreply.github.com>
2022-06-16 12:46:18 -0700

    Create Computing Fast Average.cpp

---
+++ Data Structure/Segment Tree/Computing Fast Average.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef     long long    ll;
+typedef     vector<int> vi;
+typedef     vector<long long> vl;
+typedef     pair<int, int>pi;
+typedef     pair<long long, long long>pl;
+#define F   first
+#define S   second
+#define pb  push_back
+#define     all(x)      x.begin() , x.end()
+#define mp  make_pair
+#define       FOR(i,a,b) for(i=a;i<=b;i++)
+#define     mem(a)      memset(a , 0 ,sizeof a)
+#define     memn(a)     memset(a , -1 ,sizeof a)
+ll tree[4  * 200005];
+ll a[4 * 200005];
+ll lazy[4 * 200005];
+void  built_tree(ll b, ll e, ll node)
+{
+	if (b == e) {
+		tree[node] = a[b];
+		return;
+	}
+	ll mid = (b + e) / 2;
+	built_tree(b, mid, node * 2);
+	built_tree(mid + 1, e, node * 2 + 1);
+	tree[node] = tree[node * 2] + tree[node * 2 + 1];
+}
+void update(ll node, ll b, ll e, ll l, ll r, ll val) {
+	if (lazy[node] != -1) {
+		tree[node] = (e - b + 1) * lazy[node];
+		if (b != e) {
+			lazy[node * 2] = lazy[node];
+			lazy[node * 2 + 1] = lazy[node];
+		}
+		lazy[node] = -1;
+	}
+	if (b > r || e < l)  return;
+	if (b >= l and e <= r)
+	{
+		tree[node] = (e - b + 1) * val;
+
+		if (b != e) {
+			lazy[node * 2] = val;
+			lazy[node * 2 + 1] = val;
+		}
+		lazy[node] = -1;
+		return;
+	}
+	ll mid = (b + e) / 2;
+	update(node * 2, b, mid, l, r, val);
+	update(node * 2 + 1, mid + 1, e, l, r, val);
+	tree[node] = tree[node * 2] + tree[node * 2 + 1];
+}
+ll query(ll node, ll  b, ll e, ll l, ll r)
+{
+	if (lazy[node] != -1) {
+		tree[node] = (e - b + 1) * lazy[node];
+		if (b != e) {
+			lazy[2 * node] = lazy[node];
+			lazy[2 * node + 1] = lazy[node];
+		}
+		lazy[node] = -1;
+	}
+
+	if (b > e || l > e || r < b) return 0;
+
+	if (l <= b && e <= r) return tree[node];
+	ll mid = (b + e) / 2;
+	ll ans1 = query(node * 2, b, mid, l, r);
+	ll ans2 = query(node * 2 + 1, mid + 1, e, l, r);
+	return ans1 + ans2;
+}
+int main()
+{
+	int t = 1, l = 1;
+	scanf("%d", &t);
+	while (t--)
+	{
+		mem(tree);
+		memn(lazy);
+		ll x, n, i, j, q;
+		scanf("%lld %lld", &n, &q);
+		mem(a);
+		built_tree(1, n, 1);
+		printf("Case %d:\n", l++);
+		for (i = 1; i <= q; i++)
+		{
+			ll pos, value, l, r;
+			scanf("%lld", &x);
+			if (x == 1)
+			{
+				scanf("%lld %lld %lld", &l, &r, &value);
+				l++; r++;
+				update(1, 1, n, l, r, value);
+			}
+			else
+			{
+				scanf("%lld %lld", &l, &r);
+				l++; r++;
+				ll ans = query(1, 1, n, l, r);
+				ll ln = (r - l + 1);
+				ll g = __gcd(ans, ln);
+				ans /= g; ln /= g;
+				printf("%lld", ans);
+				if (ln != 1) {
+					printf("/%lld", ln);
+				}
+				printf("\n");
+
+			}
+		}
+
+	}
+}
